a4b674ec73b8964148c43d658f2ec1d91b053377
yassua112 <silitongayosua@gmail.com>
2019-03-11 17:54:02 +0700

    versi1

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -15,11 +15,11 @@
       <div class="item-slick1 item1-slick1" style="background-image: url({{asset('assets/images/master-slide-02.jpg')}});">
         <div class="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
           <span class="caption1-slide1 m-text1 t-center animated visible-false m-b-15" data-appear="fadeInDown">
-            Profile 1
+            Women Collection 2018
           </span>
 
           <h2 class="caption2-slide1 xl-text1 t-center animated visible-false m-b-37" data-appear="fadeInUp">
-            Nama
+            New arrivals
           </h2>
 
           <div class="wrap-btn-slide1 w-size1 animated visible-false" data-appear="zoomIn">
@@ -34,11 +34,11 @@
       <div class="item-slick1 item2-slick1" style="background-image: url({{asset('assets/images/master-slide-03.jpg')}});">
         <div class="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
           <span class="caption1-slide1 m-text1 t-center animated visible-false m-b-15" data-appear="rollIn">
-            profile 2
+            Women Collection 2018
           </span>
 
           <h2 class="caption2-slide1 xl-text1 t-center animated visible-false m-b-37" data-appear="lightSpeedIn">
-            nama
+            New arrivals
           </h2>
 
           <div class="wrap-btn-slide1 w-size1 animated visible-false" data-appear="slideInUp">
@@ -53,11 +53,11 @@
       <div class="item-slick1 item3-slick1" style="background-image: url({{asset('assets/images/master-slide-04.jpg')}});">
         <div class="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
           <span class="caption1-slide1 m-text1 t-center animated visible-false m-b-15" data-appear="rotateInDownLeft">
-            profile 3
+            Women Collection 2018
           </span>
 
           <h2 class="caption2-slide1 xl-text1 t-center animated visible-false m-b-37" data-appear="rotateInUpRight">
-            nama
+            New arrivals
           </h2>
 
           <div class="wrap-btn-slide1 w-size1 animated visible-false" data-appear="rotateIn">
@@ -72,136 +72,118 @@
   </div>
 </section>
 
-<!-- New Product -->
-<section class="newproduct bgwhite p-t-45 p-b-105">
-  <div class="container">
-    <div class="sec-title p-b-60">
-      <h3 class="m-text5 t-center">
-        Featured Products
-      </h3>
-    </div>
+<!-- Content page -->
+<section class="bg p-t-55 p-b-65">
+<div class="row">
+  <div class="col-md-8 col-lg-9 p-b-75">
+    <div class = "container">
+      <div class="leftbar p-r-20 p-r-0-sm"> 
+        <div class=" p-r-50 p-r-0-lg">
+        <!-- Item Blog -->
+          <div class="item-blog p-b-80">
+				  		<a href="{{url('blog/detail/A001')}}" class="item-blog-img pos-relative dis-block hov-img-zoom">
+					  		<img src="{{asset('assets/images/blog-04.jpg')}}" alt="IMG-BLOG">
 
-<!-- Instagram -->
-<section class="instagram p-t-20">
-  <div class="sec-title p-b-52 p-l-15 p-r-15">
-    <h3 class="m-text5 t-center">
-      @ follow us on Instagram
-    </h3>
+                <span class="dis-block flex-c-m pos1 size20 bg4 s-text1">
+								Selamat datang di Website Antinomi Law Office
+							</span>	  						
+				  		</a>
+
+	  					<div class="item-blog-txt p-t-33">						  
+						  	<p class="p-b-12">
+						  		Sebagai pendiri dari Antinomi Law Office pada Tahun 2018, rupanya Ucok Tamba menyadari
+                  akan kekurangan sebagai single partner...
+						  	</p>
+
+						  	<a href="blog-detail.html" class="s-text20">
+								Continue Reading
+							  	<i class="fa fa-long-arrow-right m-l-8" aria-hidden="true"></i>
+							  </a>
+					  	</div>
+					  </div>
+        </div>  
+      </div>
+    </div>  
+  </div>      
+  <div class="col-md-4 col-lg-3 p-b-75">      
+    <!--  -->
+    <div class="rightbar">
+      <h4 class="m-text23 p-t-56 p-b-34 ">
+          Mengapa Antinomi Law Office
+      </h4>
+
+        <ul class="p-b-54">
+           <li class="p-t-4">
+              <div class="btn-group open">
+              <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" 
+                      aria-haspopup="true" aria-expanded="false">
+                             We're Reliabic Partner
+                                <span class="sr-only">
+                                </span>
+                </button>
+                <div class="dropdown-menu bg-primary">                  
+                    <a class="dropdown-item" href="#" >
+                       dropdownmenu1
+                    </a>                  
+                    <a class="dropdown-item" href="#" >
+                       dropdownmenu2
+                    </a>                 
+                    <a class="dropdown-item" href="#" >
+                       dropdownmenu3
+                    </a>                  
+                </div>               
+              </div>
+           </li>
+
+           <li class="p-t-4">
+           <div class="btn-group open">
+              <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" 
+                      aria-haspopup="true" aria-expanded="false">
+                             We're Reliabic Partner
+                                <span class="sr-only">
+                                </span>
+                </button>
+                <div class="dropdown-menu bg-primary">                  
+                    <a class="dropdown-item" href="#" >
+                       dropdownmenu1
+                    </a>                  
+                    <a class="dropdown-item" href="#" >
+                       dropdownmenu2
+                    </a>                 
+                    <a class="dropdown-item" href="#" >
+                       dropdownmenu3
+                    </a>                  
+                </div>               
+              </div>
+           </li>
+
+           <li class="p-t-4">
+              <div class="btn-group open">
+              <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" 
+                      aria-haspopup="true" aria-expanded="false">
+                             We're Reliabic Partner
+                                <span class="sr-only">
+                                </span>
+                </button>
+                <div class="dropdown-menu bg-primary">                  
+                    <a class="dropdown-item" href="#" >
+                       dropdownmenu1
+                    </a>                  
+                    <a class="dropdown-item" href="#" >
+                       dropdownmenu2
+                    </a>                 
+                    <a class="dropdown-item" href="#" >
+                       dropdownmenu3
+                    </a>                  
+                </div>               
+              </div>
+           </li>           
+        </ul>         
+     </div>
   </div>
-
-  <div class="flex-w">
-    <!-- Block4 -->
-    <div class="block4 wrap-pic-w">
-      <img src="{{asset('assets/images/gallery-03.jpg')}}" alt="IMG-INSTAGRAM">
-
-      <a href="#" class="block4-overlay sizefull ab-t-l trans-0-4">
-        <span class="block4-overlay-heart s-text9 flex-m trans-0-4 p-l-40 p-t-25">
-          <i class="icon_heart_alt fs-20 p-r-12" aria-hidden="true"></i>
-          <span class="p-t-2">39</span>
-        </span>
-
-        <div class="block4-overlay-txt trans-0-4 p-l-40 p-r-25 p-b-30">
-          <p class="s-text10 m-b-15 h-size1 of-hidden">
-            Nullam scelerisque, lacus sed consequat laoreet, dui enim iaculis leo, eu viverra ex nulla in tellus. Nullam nec ornare tellus, ac fringilla lacus. Ut sit amet enim orci. Nam eget metus elit.
-          </p>
-
-          <span class="s-text9">
-            Photo by @nancyward
-          </span>
-        </div>
-      </a>
-    </div>
-
-    <!-- Block4 -->
-    <div class="block4 wrap-pic-w">
-      <img src="{{asset('assets/images/gallery-07.jpg')}}" alt="IMG-INSTAGRAM">
-
-      <a href="#" class="block4-overlay sizefull ab-t-l trans-0-4">
-        <span class="block4-overlay-heart s-text9 flex-m trans-0-4 p-l-40 p-t-25">
-          <i class="icon_heart_alt fs-20 p-r-12" aria-hidden="true"></i>
-          <span class="p-t-2">39</span>
-        </span>
-
-        <div class="block4-overlay-txt trans-0-4 p-l-40 p-r-25 p-b-30">
-          <p class="s-text10 m-b-15 h-size1 of-hidden">
-            Nullam scelerisque, lacus sed consequat laoreet, dui enim iaculis leo, eu viverra ex nulla in tellus. Nullam nec ornare tellus, ac fringilla lacus. Ut sit amet enim orci. Nam eget metus elit.
-          </p>
-
-          <span class="s-text9">
-            Photo by @nancyward
-          </span>
-        </div>
-      </a>
-    </div>
-
-    <!-- Block4 -->
-    <div class="block4 wrap-pic-w">
-      <img src="{{asset('assets/images/gallery-09.jpg')}}" alt="IMG-INSTAGRAM">
-
-      <a href="#" class="block4-overlay sizefull ab-t-l trans-0-4">
-        <span class="block4-overlay-heart s-text9 flex-m trans-0-4 p-l-40 p-t-25">
-          <i class="icon_heart_alt fs-20 p-r-12" aria-hidden="true"></i>
-          <span class="p-t-2">39</span>
-        </span>
-
-        <div class="block4-overlay-txt trans-0-4 p-l-40 p-r-25 p-b-30">
-          <p class="s-text10 m-b-15 h-size1 of-hidden">
-            Nullam scelerisque, lacus sed consequat laoreet, dui enim iaculis leo, eu viverra ex nulla in tellus. Nullam nec ornare tellus, ac fringilla lacus. Ut sit amet enim orci. Nam eget metus elit.
-          </p>
-
-          <span class="s-text9">
-            Photo by @nancyward
-          </span>
-        </div>
-      </a>
-    </div>
-
-    <!-- Block4 -->
-    <div class="block4 wrap-pic-w">
-      <img src="{{asset('assets/images/gallery-13.jpg')}}" alt="IMG-INSTAGRAM">
-
-      <a href="#" class="block4-overlay sizefull ab-t-l trans-0-4">
-        <span class="block4-overlay-heart s-text9 flex-m trans-0-4 p-l-40 p-t-25">
-          <i class="icon_heart_alt fs-20 p-r-12" aria-hidden="true"></i>
-          <span class="p-t-2">39</span>
-        </span>
-
-        <div class="block4-overlay-txt trans-0-4 p-l-40 p-r-25 p-b-30">
-          <p class="s-text10 m-b-15 h-size1 of-hidden">
-            Nullam scelerisque, lacus sed consequat laoreet, dui enim iaculis leo, eu viverra ex nulla in tellus. Nullam nec ornare tellus, ac fringilla lacus. Ut sit amet enim orci. Nam eget metus elit.
-          </p>
-
-          <span class="s-text9">
-            Photo by @nancyward
-          </span>
-        </div>
-      </a>
-    </div>
-
-    <!-- Block4 -->
-    <div class="block4 wrap-pic-w">
-      <img src="{{asset('assets/images/gallery-15.jpg')}}" alt="IMG-INSTAGRAM">
-
-      <a href="#" class="block4-overlay sizefull ab-t-l trans-0-4">
-        <span class="block4-overlay-heart s-text9 flex-m trans-0-4 p-l-40 p-t-25">
-          <i class="icon_heart_alt fs-20 p-r-12" aria-hidden="true"></i>
-          <span class="p-t-2">39</span>
-        </span>
-
-        <div class="block4-overlay-txt trans-0-4 p-l-40 p-r-25 p-b-30">
-          <p class="s-text10 m-b-15 h-size1 of-hidden">
-            Nullam scelerisque, lacus sed consequat laoreet, dui enim iaculis leo, eu viverra ex nulla in tellus. Nullam nec ornare tellus, ac fringilla lacus. Ut sit amet enim orci. Nam eget metus elit.
-          </p>
-
-          <span class="s-text9">
-            Photo by @nancyward
-          </span>
-        </div>
-      </a>
-    </div>
-  </div>
+</div> 
 </section>
-
+ 
 
 	<!-- Footer -->
 	<footer class="bg6 p-t-45 p-b-43 p-l-45 p-r-45">
@@ -322,7 +304,7 @@
 		</div>
 			<div class="t-center s-text7 p-t-10">
 				Copyright © 2018 All rights reserved. | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-			</div>
+			</div>	
 	</footer>
 
 
@@ -339,7 +321,7 @@
 <script type="text/javascript">
 $(".selection-1").select2({
 	minimumResultsForSearch: 20,
-	dropdownParent: $('#drdpDownSelect1')
+	dropdownParent: $('#dropDownSelect1')
 });
 </script>
 <!--===============================================================================================-->

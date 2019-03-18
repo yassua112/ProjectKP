96e1aad7bef6b7cf7f715208bf2f93c3277db217
yassua112 <silitongayosua@gmail.com>
2019-03-18 12:50:19 +0700

    Penambahan CSS

--- resources/views/lawyers.blade.php
+++ resources/views/lawyers.blade.php
@@ -1,3 +1,12 @@
 @extends('layouts.app-all')
+@section('css')
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="{{asset('assets/vendor/daterangepicker/daterangepicker.css')}}">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="{{asset('assets/vendor/lightbox2/css/lightbox.min.css')}}">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="{{asset('assets/vendor/noui/nouislider.min.css')}}">
+
+@stop
 
 
